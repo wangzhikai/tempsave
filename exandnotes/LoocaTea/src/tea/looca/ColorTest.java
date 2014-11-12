5438ec8a936e59b3f2256028be574cdac74b0a70
Zhikai Wang <zwang@irise.com>
2014-11-12 11:51:04 -0800

    Intellij gitignore

---
+++ exandnotes/LoocaTea/src/tea/looca/ColorTest.java
@@ -0,0 +1,40 @@
+package tea.looca;
+
+import java.util.Calendar;
+
+public class ColorTest {
+	public static String getARandom256RGBString () {
+		String guard = "000000";
+		String freight = Integer.toHexString((int) (Math.random()*(0xffffff+1)));
+		String tmp = freight;
+		for (int i = 0; i<guard.length() - freight.length() ; i++ ) {
+			tmp = "0"+tmp;
+		}
+		return tmp;
+	}
+	public static String getAnIriseRandom256RGBString () {
+		return "#"+getARandom256RGBString ();
+	}
+	public static void main(String[] args) {
+		long start = Calendar.getInstance().getTimeInMillis();
+		Integer i = new Integer(255);
+		System.out.println(i.toHexString(i).toUpperCase());
+		System.out.println(Integer.MAX_VALUE);
+		for (int c = 0; c<=Integer.MAX_VALUE/10000; c++) {
+			String str = getAnIriseRandom256RGBString ();
+			str = str.replaceAll("#", "");
+			if (c%10000 == 0) {
+				System.out.println(str + "\t"+c);
+			}
+			int t = Integer.parseInt(str,16);
+			if (t > 0xffffff || t<0 || str.length() != 6) {
+				System.err.println("You design failed.");
+				break;
+			}
+			
+		}
+		System.out.println("It takes "+( Calendar.getInstance().getTimeInMillis()- start) +" milli-seconds");
+
+	}
+
+}
---
+++ exandnotes/LoocaTea/src/tea/looca/PatchHex.java
@@ -0,0 +1,14 @@
+package tea.looca;
+
+public class PatchHex {
+
+	public static void main(String[] args) {
+		String s = "PATCH";
+		for (int i = 0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			System.out.println(Integer.toHexString(c));
+			
+		}
+	}
+
+}
