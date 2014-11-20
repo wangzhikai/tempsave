eb1617fa4fdc65d6bd09d1d3a44fed872d5c4803
Zhikai Wang <zwang@irise.com>
2014-11-20 09:42:31 -0800

    20141119

---
+++ exandnotes/LoocaTea/src/tea/looca/CartesianProductTest.java
@@ -0,0 +1,10 @@
+package tea.looca;
+import java.util.Collection.metaClass.cartesianProduct;
+public class CartesianProductTest {
+
+	public static void main(String[] args) {
+		java.util.Collection.metaClass.cartesianProduct cp;
+
+	}
+
+}
