313a719fcefe6d1a9a18914be826522acb81dbad
Zhikai Wang <zwang@irise.com>
2014-12-03 13:48:34 -0800

    Good developer TESTs it first.

--- exandnotes/LoocaTea/src/tea/looca/NullStringEquasTest.java
+++ exandnotes/LoocaTea/src/tea/looca/NullStringEquasTest.java
@@ -6,7 +6,21 @@ public class NullStringEquasTest {
 		String s1 = null;
 		String s2 = null;
 		System.out.println(s1==s2);
-		System.out.println(s1.equals( s2));
+		//System.out.println(s1.equals( s2));
+		
+		String s3 = "Good developer TESTs it first.";
+		String s4 = new String(s3);
+		String s5 = s3;
+		System.out.println ("s3 == s4 : " + (s3 == s4)); // must bracket s3 == s4
+		System.out.println ("s3.equals(s4) : " + s3.equals(s4));
+		System.out.println ("s3 == s5 : " + (s3 == s5));
+		System.out.println ("s3.equals(s5) : " + s3.equals(s5));
+		/*OUTPUT 
+s3 == s4 : false
+s3.equals(s4) : true
+s3 == s5 : true
+s3.equals(s5) : true
+		 */
 
 		   /*
 		//TODO Experiment about double equal.Remove from git branch. This groovy '==' is not very trust-worthy
