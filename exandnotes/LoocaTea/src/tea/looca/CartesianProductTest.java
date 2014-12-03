738b20c5b07b84f61b033206d727b3274d7d71f7
Zhikai Wang <zwang@irise.com>
2014-12-03 13:17:57 -0800

    20141103

--- exandnotes/LoocaTea/src/tea/looca/CartesianProductTest.java
+++ exandnotes/LoocaTea/src/tea/looca/CartesianProductTest.java
@@ -1,9 +1,54 @@
 package tea.looca;
-import java.util.Collection.metaClass.cartesianProduct;
+
+import java.util.LinkedList;
+import java.util.List;
+
+//import java.util.Collection.metaClass.cartesianProduct;
 public class CartesianProductTest {
+    public static List getListListCartesian (List list1, List list2) {
+        List result = new LinkedList();
+        int lastRowSize = -1;
+        for (Object e1: list1) {
+            for (Object e2: list2 ){
+                Object currentRow = new LinkedList();
+                // e1 instance of List else
+                if (e1 instanceof List) {
+                    for (Object e: (List)e1 ) {
+                        ((List)currentRow).add(e);
+                    }
+                } else {
+                	((List)currentRow).add(e1);
+                }
+
+                // e2 instance of List else
+                if (e2 instanceof List) {
+                    for (Object e: (List)e2 ) {
+                    	((List)currentRow).add(e);
+                    }
+                } else {
+                	((List)currentRow).add(e2);
+                }
+
+                if (lastRowSize != -1) {
+                    //assertEquals(lastRowSize,((List)currentRow).size(),"Matrices dimensions not match.")
+                	if (lastRowSize !=((List)currentRow).size())
+                		throw new RuntimeException("Matrices dimensions not match.");
+                }
+                lastRowSize = ((List)currentRow).size();
+                result.add(currentRow);
+            }
+        }
+        return result;
+    }
 
 	public static void main(String[] args) {
-		java.util.Collection.metaClass.cartesianProduct cp;
+		// experiments cases:
+		
+		//TODO 
+		// Case I: list of literals X list of literals
+		// Case II: list of literals X list of list
+		// Case III: list of list X list of literals
+		// Case IV: list of list X list of list
 
 	}
 
---
+++ exandnotes/LoocaTea/src/tea/looca/NullStringEquasTest.java
@@ -0,0 +1,28 @@
+package tea.looca;
+
+public class NullStringEquasTest {
+
+	public static void main(String[] args) {
+		String s1 = null;
+		String s2 = null;
+		System.out.println(s1==s2);
+		System.out.println(s1.equals( s2));
+
+		   /*
+		//TODO Experiment about double equal.Remove from git branch. This groovy '==' is not very trust-worthy
+		String s1
+		String s2
+		log.info "s1 ? s2 = "+(s1==s2)
+
+
+		public static void main(String[] args) {
+		    String s1 = null;
+		    String s2 = null;
+		    System.out.println(s1==s2);
+		    System.out.println(s1.equals( s2));
+
+		}
+		*/
+	}
+
+}
