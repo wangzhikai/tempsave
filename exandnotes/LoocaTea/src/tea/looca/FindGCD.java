ba6e6755a970ff3e73a3aef6bc6e2519aa994122
Zhikai Wang <zwang@irise.com>
2015-01-05 17:54:33 -0800

    FindGCDv3

--- exandnotes/LoocaTea/src/tea/looca/FindGCD.java
+++ exandnotes/LoocaTea/src/tea/looca/FindGCD.java
@@ -8,20 +8,27 @@ package tea.looca;
 //WatchOUT !! http://en.wikipedia.org/wiki/Modulo_operation
 //When either a or n is negative, the naive definition breaks down and programming languages differ in how these values are defined.
 
+// So far 20150105, I did not find doc for Java modulo operator. Somewhere in the source?
+// So findGCDv3 is based on black box tests.
 
 public class FindGCD {
-	public static int findGCDv2(int a, int b) {
-		if (a < b)
-			return findGCDv2(b, a);
-		else {
-			if (a % b == 0) {
-				return b;
-			} else {
-				return findGCDv2(b, a % b);
-			}
+	public static int findGCDv3(int a, int b) {
+		if (a % b == 0) {
+			return b>0?b:-b;
+			//wrong return 0x7fffffff & b;
+		} else {
+			return findGCDv3(b, a % b);
 		}
-
 	}
+	
+	public static int findGCDv2(int a, int b) {
+		if (a % b == 0) {
+			return b;
+		} else {
+			return findGCDv2(b, a % b);
+		}
+	}
+
 	public static int findGCD(int a, int b) {
 		if (a < b)
 			return findGCD(b, a);
@@ -36,38 +43,81 @@ public class FindGCD {
 	}
 
 	public static void main(String[] args) {
+		
 		{
-			//Remainder operator java
+			// Remainder operator of java <%>
+			System.out.println("===0===");
+			System.out.println(findGCDv3(882, 756));
+			 System.out.println(findGCDv3(882, -756));
+			 System.out.println(findGCDv3(-882, 756));
+			 System.out.println(findGCDv3(-882, -756));
+			 System.out.println(findGCDv3(756, 756));
+			 System.out.println(findGCDv3(756, 882));
+			 System.out.println(findGCDv3(756, -756));
+			 System.out.println(findGCDv3(-756, -756));
+			 System.out.println(findGCDv3(-756, 756));
+//			 126
+//			 126
+//			 126
+//			 126
+//			 756
+//			 126
+//			 756
+//			 756
+//			 756
+
+		}
+		{
+			// Remainder operator java
 			System.out.println("======");
 			System.out.println(882 % 756);
 			System.out.println(882 % -756);
 			System.out.println(-882 % 756);
 			System.out.println(-882 % -756);
-//			126
-//			126
-//			-126
-//			-126
+			// 126
+			// 126
+			// -126
+			// -126
 			System.out.println("======");
-			System.out.println(-756 % 882 );
-			System.out.println(-756 % -882 );
-			System.out.println(756 % 882 );
-			System.out.println(756 % -882 );
-//			-756
-//			-756
-//			756
-//			756
+			System.out.println(-756 % 882);
+			System.out.println(-756 % -882);
+			System.out.println(756 % 882);
+			System.out.println(756 % -882);
+			// -756
+			// -756
+			// 756
+			// 756
+			System.out.println("======");
+			System.out.println(findGCDv2(882, 756));
+			 System.out.println(findGCDv2(882, -756));
+			 System.out.println(findGCDv2(-882, 756));
+			 System.out.println(findGCDv2(-882, -756));
+			 System.out.println(findGCDv2(756, 756));
+			 System.out.println(findGCDv2(756, 882));
+			 System.out.println(findGCDv2(756, -756));
+//			 126
+//			 126
+//			 -126
+//			 -126
+//			 756
+//			 126
+//			 -756
+//			 System.out.println(findGCDv2(-882, 0));
+//			 Exception in thread "main" java.lang.ArithmeticException: / by zero
+//				at tea.looca.FindGCD.findGCDv2(FindGCD.java:13)
+//				at tea.looca.FindGCD.main(FindGCD.java:62)
 
 		}
-		
+
 		{
 			System.out.println("======");
 			System.out.println(findGCD(882, 756));
-//			System.out.println(findGCD(882, -756));
-//			System.out.println(findGCD(-882, 756));
-//			System.out.println(findGCD(756, 756));
-//			System.out.println(findGCD(756, 882));
-//			System.out.println(findGCD(756, -756));
-//			System.out.println(findGCD(-882, 0));
+			// System.out.println(findGCD(882, -756));
+			// System.out.println(findGCD(-882, 756));
+			// System.out.println(findGCD(756, 756));
+			// System.out.println(findGCD(756, 882));
+			// System.out.println(findGCD(756, -756));
+			// System.out.println(findGCD(-882, 0));
 			// 126
 			// Exception in thread "main" java.lang.StackOverflowError
 			// at tea.looca.FindGCD.findGCD(FindGCD.java:5)
