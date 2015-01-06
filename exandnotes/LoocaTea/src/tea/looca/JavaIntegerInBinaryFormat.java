e79ff01423b52506eb5f4e25fa851441fdb6a298
Zhikai Wang <zwang@irise.com>
2015-01-06 12:09:18 -0800

    Compute ABS of negative 32-bit signed integer

--- exandnotes/LoocaTea/src/tea/looca/JavaIntegerInBinaryFormat.java
+++ exandnotes/LoocaTea/src/tea/looca/JavaIntegerInBinaryFormat.java
@@ -5,10 +5,21 @@ public class JavaIntegerInBinaryFormat {
 
 	public static void main(String[] args) {
 		{
-			System.out.println("======");
-			for (int i = 0, j= 0; j < 8; i--,j++) {
-				System.out
-				.println(String.format("%32s", Integer.toBinaryString(i)));
+			System.out.println("===Compute ABS of negative 32-bit signed integer===");
+			for (int i = -1, j= 0; j < 8; i--,j++) {
+				//System.out.println(String.format("%32s", Integer.toBinaryString(i)));
+				System.out.println(i);
+				System.out.println(PaddingStrings.padAString(Integer.toBinaryString(i), '0', -32));
+				int tmp = i;
+				
+				//System.out.println(PaddingStrings.padAString(Integer.toBinaryString(~tmp), '0', -32));
+				//an wei qu fan zai jia yis
+				System.out.println(~tmp + 1);
+				System.out.println(PaddingStrings.padAString(Integer.toBinaryString(~tmp + 1), '0', -32));
+				
+				System.out.println();
+				
+				
 			}
 
 		}
---
+++ exandnotes/LoocaTea/src/tea/looca/TODOCollectionReverse.java
@@ -0,0 +1,21 @@
+package tea.looca;
+// TODO import org.apache.commons.lang.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TODOCollectionReverse {
+
+	public static void main(String[] args) {
+		char [] array = "abcdefg".toCharArray();
+		//Character [] carray = "abcdefg".toCharArray();
+		List l = Arrays.asList(array);
+		System.out.println(l.toString());
+		//Collections.reverse(new ArrayList(array).);
+		
+
+	}
+
+}
