2c03ffc53e47f70e121570b10a91df3e6438385d
Zhikai Wang <zwang@irise.com>
2015-01-29 13:39:11 -0800

    The overture of shark auf

--- exandnotes/LoocaTea/src/tea/looca/TODOSelimphore.java
+++ exandnotes/LoocaTea/src/tea/looca/TODOSelimphore.java
@@ -10,20 +10,26 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class TODOSelimphore {
-	//TODO https://sites.google.com/a/chromium.org/chromedriver/getting-started
-	
-	//TODO selenium-server-standalone-2.44.0.jar
+	//TO-DO https://sites.google.com/a/chromium.org/chromedriver/getting-started
+	//TO-DO selenium-server-standalone-2.44.0.jar
+	//TODO Overture
+	//TODO Shake off
 
 	public static void main(String[] args) throws InterruptedException {
 		  // Optional, if not specified, WebDriver will search your path for chromedriver.
 		  System.setProperty("webdriver.chrome.driver", "/Users/zwang/Downloads/chromedriver");
 
 		  WebDriver driver = new ChromeDriver();
-		  driver.get("http://www.google.com/xhtml");
+		  //driver.get("http://www.google.com/xhtml");
+		  //http://stackoverflow.com/questions/16215062/selenium-ide-local-non-webhosted-file-access-for-testing
+		  driver.get("file:////Users/zwang/Desktop//background-comp6361-concordia.pdf");
+		  //file///c:/path/to/your/file.html
+		  //background-comp6361-concordia.pdf
+		  
 		  Thread.sleep(5000);  // Let the user actually see something!
-		  WebElement searchBox = driver.findElement(By.name("q"));
-		  searchBox.sendKeys("ChromeDriver");
-		  searchBox.submit();
+		 // WebElement searchBox = driver.findElement(By.name("q"));
+		  //searchBox.sendKeys("ChromeDriver");
+		  //searchBox.submit();
 		  Thread.sleep(5000);  // Let the user actually see something!
 		  driver.quit();
 	}
