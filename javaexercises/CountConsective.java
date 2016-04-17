4dfd8eca66cbfb2898f5dc2eb4cd3bce786c6dd9
Zhikai Wang <wangzhikai@yahoo.com>
2016-04-17 15:31:48 -0700

    Anagram, C combination code to Java, index permutation with Java

---
+++ javaexercises/CountConsective.java
@@ -0,0 +1,45 @@
+/**
+ * @author Zhikai Wang/www.heteroclinic.net
+ * This class is an example/exercise to provide a code base to compute k-combinations of a set, using functional programming features.
+ * Given third party license acknowledged and this header kept or quoted, you can utilize this program at will.
+*/
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CountConsective {
+
+	   public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+
+	        final String data = "aabbaadddc";
+	        
+	        List<String> chars = new LinkedList<String> ();
+	        List<Integer> counts = new LinkedList<Integer> ();
+	        
+	        String  prev = "-1";    
+	        int segs = 0;    
+	        int currentCount = 0;
+	        for (int i = 0; i< data.length(); i++ ) {
+	            int currentChar = data.charAt(i);
+	            if (!( Character.toString(data.charAt(i)) ).equals(prev)) {
+	                chars.add(""+Character.toString(data.charAt(i)));
+	                segs++;
+	                currentCount = 1;
+	                 counts.add(currentCount);
+	            } else {
+	              int temp = counts.get(segs-1) +1;
+	              counts.set(segs-1,temp);
+	            }
+	            prev = Character.toString(data.charAt(i));
+	        
+	        }
+	        
+	        //OUTPUT
+	        for (int i = 0; i< chars.size(); i++ ) {
+	            System.out.print(""+chars .get (i) +counts.get(i));
+	        }
+	        
+	        
+	    }
+}
